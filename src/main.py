80406f13c2bf61c5457641b65312cef13e8cb247
Super Zombi <super.zombi.yt@gmail.com>
2025-06-30 19:07:29 +0300

    0.6.0
    * Support Voiceovers
    * Support Flag Icons

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from tkinter.filedialog import askdirectory
 from utils import *
 
 MODS_DATA = {}
-__version__ = "0.5.2"
+__version__ = "0.6.0"
 @eel.expose
 def app_version(): return __version__
 LOCALES = load_locales()
