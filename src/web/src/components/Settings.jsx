d8f9ed4b160e6ae3f4a58835f8a12782b0df6781
Super Zombi <super.zombi.yt@gmail.com>
2025-11-29 18:10:34 +0000

    fix popup

--- src/web/src/components/Settings.jsx
+++ src/web/src/components/Settings.jsx
@@ -24,84 +24,74 @@ const Settings = ({
 		setCacheSize(0)
 		setCachedMods([])
 	}
-	const onPopupClick = e => {
-		if (e.target.classList.contains("popup")){
-			onClose()
-		}
-	}
 
 	return (
-		<div className="popup show" onClick={onPopupClick}>
-			<div className="popup_wraper">
-				<img src="/images/close.svg" className="close hover" draggable={false}
-					onClick={onClose}
+		<Popup onClose={onClose}>
+			<h3 align="center"><LANG id="settings"/></h3>
+			<p style={{textAlign: "center"}}>
+				<LANG id="version"/> <span id="app-version">{appVersion}</span>
+			</p>
+			<hr/>
+			<br/>
+			<div className="flex-row">
+				<img src="/images/planet.svg" height="20" draggable={false}/>
+				<span><LANG id="language"/>:</span>
+				<Select
+					options={langOptions}
+					value={{path: language, title: langOptions.find(x=>x.path==language).title}}
+					onChange={val=>setLanguage(val.path)}
+					placeholder={langData["language"]}
 				/>
-				<h3 align="center"><LANG id="settings"/></h3>
-				<p style={{textAlign: "center"}}>
-					<LANG id="version"/> <span id="app-version">{appVersion}</span>
-				</p>
-				<hr/>
-				<br/>
-				<div className="flex-row">
-					<img src="/images/planet.svg" height="20" draggable={false}/>
-					<span><LANG id="language"/>:</span>
-					<Select
-						options={langOptions}
-						value={{path: language, title: langOptions.find(x=>x.path==language).title}}
-						onChange={val=>setLanguage(val.path)}
-						placeholder={langData["language"]}
+			</div>
+			<br/>
+			<div className="flex-center">
+				<label className="hover">
+					<input type="checkbox" className="setting_element hover"
+						checked={matchClientLang} onChange={e=>setMatchClientLang(e.target.checked)}
 					/>
-				</div>
-				<br/>
-				<div className="flex-center">
-					<label className="hover">
-						<input type="checkbox" className="setting_element hover"
-							checked={matchClientLang} onChange={e=>setMatchClientLang(e.target.checked)}
-						/>
-						<LANG id="match_client_lang"/>
-					</label>
-				</div>
-				<br/>
-				<hr/>
-				<p className="flex-center">
-					<label className="hover">
-						<input type="checkbox" className="setting_element hover"
-							checked={useCache} onChange={e=>setUseCache(e.target.checked)}
-						/>
-						<LANG id="use_cache"/>
-					</label>
-				</p>
-				<p style={{textAlign: "center"}}>
-					<span><LANG id="cache_size"/>: </span>
-					<span className="file_size">
-						<span>{cacheSize}</span>
-						<LANG id="megabytes"/>
-					</span>
-				</p>
-				<div style={{textAlign: "center", fontSize: "14px"}}>
-					<div className="button hover" onClick={clear_cache}>
-						<LANG id="clear_cache"/>
-					</div>
-				</div>
-				<hr/>
-				<p className="links">
-					<a href="https://www.youtube.com/c/SuperZombi" className="hover" target="_blank">
-						<img src="/images/youtube_logo.png" draggable={false}/>
-					</a>
-					<a href="https://github.com/SuperZombi/wot-modpack" className="hover" target="_blank">
-						<img src="/images/github_logo.png" draggable={false}/>
-					</a>
-					<a href="https://donatello.to/super_zombi" className="hover" target="_blank">
-						<img src="/images/donatello_logo.png" draggable={false}/>
-					</a>
-				</p>
-				<div style={{textAlign: "center", fontSize: "12px"}}>
-					<a className="button hover" href="https://github.com/SuperZombi/wot-modpack/issues" target="_blank">
-						<LANG id="report_bug"/>
-					</a>
+					<LANG id="match_client_lang"/>
+				</label>
+			</div>
+			<br/>
+			<hr/>
+			<p className="flex-center">
+				<label className="hover">
+					<input type="checkbox" className="setting_element hover"
+						checked={useCache} onChange={e=>setUseCache(e.target.checked)}
+					/>
+					<LANG id="use_cache"/>
+				</label>
+			</p>
+			<p style={{textAlign: "center"}}>
+				<span><LANG id="cache_size"/>: </span>
+				<span className="file_size">
+					<span>{cacheSize}</span>
+					<LANG id="megabytes"/>
+				</span>
+			</p>
+			<div style={{textAlign: "center", fontSize: "14px"}}>
+				<div className="button hover" onClick={clear_cache}>
+					<LANG id="clear_cache"/>
 				</div>
 			</div>
-		</div>
+			<hr/>
+			<p className="links">
+				<a href="https://www.youtube.com/c/SuperZombi" className="hover" target="_blank">
+					<img src="/images/youtube_logo.png" draggable={false}/>
+				</a>
+				<a href="https://github.com/SuperZombi/wot-modpack" className="hover" target="_blank">
+					<img src="/images/github_logo.png" draggable={false}/>
+				</a>
+				<a href="https://donatello.to/super_zombi" className="hover" target="_blank">
+					<img src="/images/donatello_logo.png" draggable={false}/>
+				</a>
+			</p>
+			<div style={{textAlign: "center", fontSize: "12px"}}>
+				<a className="button hover" href="https://github.com/SuperZombi/wot-modpack/issues" target="_blank">
+					<LANG id="report_bug"/>
+				</a>
+			</div>
+		</Popup>
 	)
 }
 function bytesToMb(bytes) {
--- src/web/src/components/UpdatePopup.jsx
+++ src/web/src/components/UpdatePopup.jsx
@@ -1,20 +1,28 @@
+const Popup = ({onClose, children}) => {
+	return (
+		<div className="popup show" onClick={e=>e.target.classList.contains("popup") ? onClose() : null}>
+			<div className="popup_wraper">
+				<img src="/images/close.svg" className="close hover" draggable={false}
+					onClick={onClose}
+				/>
+				{children}
+			</div>
+		</div>
+	)
+}
+
 const UpdatePopup = ({
 	onClose
 }) => {
 	return (
-		<div className="popup show">
-			<div className="popup_wraper" style={{height: "fit-content"}}>
-				<img src="/images/close.svg" className="close hover" draggable={false}
-					onClick={onClose}
-				/>
-				<div className="flex-center">
-					<img src="/images/up-arrow.svg" height="64" draggable={false} style={{userSelect: "none"}}/>
-					<h3 align="center"><LANG id="update_available"/></h3>
-					<a className="button hover" href="https://github.com/SuperZombi/wot-modpack/releases" target="_blank">
-						<LANG id="download"/>
-					</a>
-				</div>
+		<Popup onClose={onClose}>
+			<div className="flex-center">
+				<img src="/images/up-arrow.svg" height="64" draggable={false} style={{userSelect: "none"}}/>
+				<h3 align="center"><LANG id="update_available"/></h3>
+				<a className="button hover" href="https://github.com/SuperZombi/wot-modpack/releases" target="_blank">
+					<LANG id="download"/>
+				</a>
 			</div>
-		</div>
+		</Popup>
 	)
 }
